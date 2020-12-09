602346c9df3082ac456a8547bb717a885a75fca9
tutubunso <aymardbutungane@gmail.com>
2020-12-09 11:56:54 +0200

    ajout des urls

--- ijamboAdmin/urls.py
+++ ijamboAdmin/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add_event/', views.event, name="addevent"),
+    path('indexer/', views.indexe, name="indexer"),
+    path('suggestion/', views.contact, name="contact"),
+    path('payer/<int:id>/', views.paiement,name="paiement"),
+    path('liste/',views.listevents,name="listevents"),
+    path('supprimer/<int:id>/', views.delete,name="supprimerE"),
     
 ]
